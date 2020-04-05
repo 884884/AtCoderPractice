ad41c5509bf2d7bccf3b8149c2ca40140fc6e8d4
mune <884m884@gmail.com>
2020-04-05 14:02:08 +0900

    no message

--- abc041_a.go
+++ abc041_a.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// 以下の問題
+// https://atcoder.jp/contests/abc041/submissions/me
+
 func main() {
 	var (
 		s string
@@ -9,5 +12,11 @@ func main() {
 	)
 	fmt.Scan(&s, &i)
 
-	fmt.Println(s[i])
+	// 1バイト（シングルバイト）文字なら以下でOK
+	// fmt.Println(s[i-1],s[i])
+
+	// 1バイトより多いマルチバイト文字なら,rune型で扱う
+	// []rune()でstringからruneへキャストする
+	runeSlice := []rune(s)
+	fmt.Println(string(runeSlice[i-1]))
 }
--- answering/main.go
+++ answering/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("aaa")
+// 以下の問題
+
+func main() {
+	var (
+		s string
+		i int
+	)
+	fmt.Scan(&s, &i)
+
+	// 1バイト（シングルバイト）文字なら以下でOK
+	// fmt.Println(s[i-1],s[i])
+
+	// 1バイトより多いマルチバイト文字なら,rune型で扱う
+	// []rune()でstringからruneへキャストする
+	runeSlice := []rune(s)
+	fmt.Println(string(runeSlice[i-1]))
 }
